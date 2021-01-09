949a3e8ee681f2dd95bccd848759502d22e12d94
rijoblockchain <rijoblockchain@gmail.com>
2021-01-10 00:33:44 +0530

    added modifiers

--- contracts/KYC_Phase1.sol
+++ contracts/KYC_Phase1.sol
@@ -18,31 +18,37 @@ contract KYCPhase1 {
     mapping(bytes32 => Customer) customerList;
     
     
-    function addCustomer(bytes32  _userName, bytes32  _customerData) public returns(bool) {
-        require(customerList[_userName].userName == 0, "Customer already present");
+    function addCustomer(bytes32  _userName, bytes32  _customerData) external customerNotPresent(_userName) returns(bool) {
         customerList[_userName].userName = _userName; 
         customerList[_userName].customerData = _customerData; 
         return true;
     } 
     
-    function removeCustomer(bytes32  _userName) public returns(bool) {
-        require(customerList[_userName].userName != 0, "Customer not present");
+    function removeCustomer(bytes32  _userName) external customerPresent (_userName) returns(bool) {
         delete customerList[_userName];
         return true;
         
     }
     
-    function modifyCustomer(bytes32  _userName, bytes32  _newCustomerData) public returns(bool) {
-        require(customerList[_userName].userName != 0, "Customer not present");
+    function modifyCustomer(bytes32  _userName, bytes32  _newCustomerData) external customerPresent (_userName) returns(bool) {
         customerList[_userName].customerData = _newCustomerData; 
         return true;
     }
     
-    function viewCustomer(bytes32 _userName) public view returns(bytes32) {
-        require(customerList[_userName].userName != 0, "Customer not present");
+    function viewCustomer(bytes32 _userName) external customerPresent (_userName) view returns(bytes32) {
         return customerList[_userName].customerData;
     }
     
     
+    modifier customerNotPresent (bytes32 _userName) {
+        require(customerList[_userName].userName == 0, "Customer already present");
+        _;
+        
+    }
     
+    modifier customerPresent (bytes32 _userName) {
+        require(customerList[_userName].userName != 0, "Customer not present");
+        _;
+        
+    }
 }
